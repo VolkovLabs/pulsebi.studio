171dd764880df51d43e0a267e9a908a0b330a098
Mikhail Volkov <mikhail@volkovlabs.io>
2025-09-09 15:23:03 -0400

    Updates

--- config/announcement.ts
+++ config/announcement.ts
@@ -4,7 +4,7 @@
 export const ANNOUNCEMENT_BAR = {
   id: "business_intelligence_3_8_0",
   content:
-    '<a href="/blog/business-intelligence-3.8.0/">Business Intelligence 3.8.0: Redefining Data-Driven Excellence</a> is out 🚀',
+    '<a href="/blog/business-intelligence-3.8.0/">Business Intelligence 3.8.0: Elevating Data-Driven Decision Making</a> is out 🚀',
   backgroundColor: "#16617f",
   textColor: "#FFFFFF",
 };
